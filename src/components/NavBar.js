f58e110fc0d4457613215a8a5352b15754b98ca2
Dillon Luther <dillon.luther@gmail.com>
2020-12-28 18:14:46 -0700

    padding

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -23,10 +23,10 @@ export const NavBar = () => (
         <h3 className='product-type nav-option'>Sets</h3>
       </div>
       <div className='right-nav flexed-ai-center'>
-        <i className="fas fa-search"></i>
+        <i className="nav-option fas fa-search"></i>
         <h3 className='nav-option'>Language</h3>
         <h3 className='nav-option'>Sign In</h3>
-        <i className="fas fa-shopping-cart"></i>
+        <i className="nav-option fas fa-shopping-cart"></i>
       </div>
     </div>
   </div>
