1269689c3dbbece8fb7486f0e0f36f4ee507f1ba
Dillon Luther <dillon.luther@gmail.com>
2020-12-29 14:39:55 -0700

    finished abstracting product details

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,7 +1,5 @@
 import React, { useState } from "react";
 import redKnife from '../assets/chef_knife_red.webp'
-// import engravingHandle from '../assets/engraving_handle.webp'
-// import engravingBlade from '../assets/engraving_blade.webp'
 import { ProductDetails } from "./ProductDetails";
 
 export const Product = () => {
@@ -33,42 +31,6 @@ export const Product = () => {
     }
   }
 
-  const updateSelect = e => {
-    const selected = e.target;
-    const newVal = selected.textContent;
-    const selectMenu = document.querySelector('#select-menu');   
-    // remove selected highlight
-    document.querySelectorAll('.select-option').forEach(el => {
-      el.classList.remove('selected');
-    })
-    // add to the new one
-    selected.classList.add('selected');
-    const chosenQty = document.getElementById('chosen-qty');
-    chosenQty.textContent = newVal;
-    const dropdown = document.querySelector('#select-dropdown');   
-    selectMenu.classList.remove('active');
-    dropdown.classList.add('undisplayed')
-
-  }
-
-  const toggleDropdown = () => {
-    const dropdown = document.querySelector('#select-dropdown');   
-    const selectMenu = document.querySelector('#select-menu');   
-    const purchaseActions = document.querySelector('#purchase-options');
-    const selectChevron = document.querySelector('#select-chevron');
-    if (dropdown.classList.contains('undisplayed')) {
-      dropdown.classList.remove('undisplayed');
-      selectMenu.classList.add('active');
-      purchaseActions.classList.add('space-bottom');
-      selectChevron.classList = 'fas fa-chevron-up';
-    } else {
-      dropdown.classList.add('undisplayed');
-      selectMenu.classList.remove('active');
-      purchaseActions.classList.remove('space-bottom');
-      selectChevron.classList = 'fas fa-chevron-down';
-    }
-  }
-
   return (
     <div id='product'>
       <div className='picture-container'>
@@ -86,9 +48,6 @@ export const Product = () => {
                       engravingLength={engravingLength}
                       updateEngraving={updateEngraving}
                       togglePersonalizeCheckbox={togglePersonalizeCheckbox}
-                      updateSelect={updateSelect}
-                      toggleDropdown={toggleDropdown}
-
       />
     </div>
   )
